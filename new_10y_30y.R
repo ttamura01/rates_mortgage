cb5bc534610ed8a26945def5d938e7fd5997339f
ttamura01git config --global user.email ttamura010164@me.com~git remote add origin https://github.com/ttamura01/git-fed_rate_cut.gitgit push origin mainls <ttamura010164@me.com>
2025-12-05 08:29:14 -0500

    weekly updates with the latest data

--- new_10y_30y.R
+++ new_10y_30y.R
@@ -19,7 +19,7 @@ tail(treasury_10y)
 # treasury_10y <- treasury_10y[-708,]
 
 updates <- tribble(~date, ~long_term_yield,
-                   "2025-11-20", 4.088)
+                   "2025-12-04", 4.102)
 
 updates$date <- as.Date(updates$date)
 
