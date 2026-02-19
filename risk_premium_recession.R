01e647bfcbee1428f8a79b6614a7bbd5a4e4f8c0
ttamura01git config --global user.email ttamura010164@me.com~git remote add origin https://github.com/ttamura01/git-fed_rate_cut.gitgit push origin mainls <ttamura010164@me.com>
2026-02-18 20:03:26 -0500

    updated the codes with recession

--- risk_premium_recession.R
+++ risk_premium_recession.R
@@ -6,6 +6,7 @@ library(glue)
 library(quantmod)   # for Fed Funds
 library(fredr) 
 library(plotly)
+library(patchwork)
 
 
 #Set my FRED API key
@@ -114,9 +115,9 @@ long_data %>%
   annotate("label",
            x = as.Date("2010-01-01"), 
            y = 15,
-    label = "Spread:\nmean = 1.66%\nsd = 0.54%\n
+           label = "Spread:\nmean = 1.66%\nsd = 0.54%\n
           mean + sd = 2.2%\nmean + 2sd = 2.74%",
-  fontface = "bold") +
+           fontface = "bold") +
   annotate("text",
            x = initial_date,
            y = mean_spread,
@@ -173,9 +174,9 @@ avg_high_yield <- round(avg_rates$avg_high_yield, 3)
 spread_comp <- avg_rates %>% 
   select(date, spread, high_yield) %>% 
   pivot_longer(-date, names_to = "assets", values_to = "spread")
-  
 
-spread_comp %>% 
+
+p <- spread_comp %>% 
   na.omit() %>% 
   ggplot(aes(x = date, y = spread, colour = assets))+
   geom_line(show.legend = FALSE) +
@@ -191,7 +192,7 @@ spread_comp %>%
            label = "30-Year Mortgage Spread") +
   scale_color_manual(breaks = c("spread", "high_yield"),
                      values = c("red", "blue")
-                     ) +
+  ) +
   scale_y_continuous(limits = c(0, NA),
                      breaks = seq(0, 20, 5),
                      labels = label_comma(accuracy = 0.1)) +
@@ -205,196 +206,75 @@ spread_comp %>%
     plot.title = element_textbox_simple(size = 16, margin = margin(b=10, t=5)),
     plot.subtitle = element_textbox_simple()
   )
+p
 ggsave("figures/us_30y_hi_yield_spread.png", height = 6, width = 6)  
 
 
-
-
-##historical spread with latest data
-rates$latest_data <- ifelse(rates$date == max(rates$date), TRUE, FALSE)
-
-rates %>% 
-  ggplot(aes(x = date, y = spread, color = latest_data, fill = latest_data)) +
-  geom_col(show.legend = FALSE) +
-  scale_colour_manual(breaks = c(TRUE, FALSE),
-                    values = c("red", "#AAAAAA")) +
-  geom_text(data = subset(rates, latest_data == TRUE),
-            aes(label = glue("{spread}%")), vjust=-.5, hjust = 0.5, color = "red") +
-  labs(title = "US 30-year mortgage spread over 10-year treasury yield",
-       x = NULL, y = NULL)
-
-ggsave("figures/historical_spread_latest_data.png", height = 4.5, width = 5)
   
-##boxplot
-
-rates$year <- format(rates$date, "%Y")
-sapply(rates, class)
-rates$year = as.numeric(rates$year)
-
-rates$period <- cut(rates$year,
-                    breaks = c(1970, 2008, 2009, 2019, 2024, 2025),
-                    labels = c("1971-2008", "2009-2019", "2020-2021", "2022-2024", "2025"))
-
-rates %>% 
-  drop_na(period) %>% 
-  ggplot(aes(x = period, y = spread, fill = period)) + 
-  geom_boxplot(show.legend = FALSE) +
-  # facet_wrap(~period) +
-  scale_y_continuous(limits = c(-0.5, 6),
-                     breaks = seq(0, 6, 2),
-                     labels = label_comma(accuracy = 0.1)) +
-  labs(title = "30 years mortgage yield spread over 10 years treasuary notes",
-          subtitle = "The spread widened since 2022, with unwinding of MBS by Fed?", 
-          y = "spread",
-          x = NULL) +
-  theme(plot.title.position = "plot",
-        plot.subtitle = element_text(face = "italic"))
-
-# d <- ggplot(rates, aes(x = period, y = spread, fill = period)) +
-#   geom_boxplot(show.legend = FALSE) +
-#   # facet_wrap(~period) +
-#   scale_y_continuous(limits = c(-0.5, 6),
-#                      breaks = seq(0, 6, 2),
-#                      labels = label_comma(accuracy = 0.1)) +
-#   labs(x = NULL,
-#        y = "spread")
-# 
-# 
-# d +  labs(title = "30 years mortgage yield spread over 10 years treasuary notes",
-#           subtitle = "The spread widened since 2022, with unwinding of MBS by Fed?", 
-#           y = "spread",
-#           x = NULL) +
-#   theme(plot.title.position = "plot",
-#         plot.subtitle = element_text(face = "italic"))
-
-ggsave("figures/boxplots of spread indicating some impacts from Fed's MBS unwinding.png", width = 5, height = 4)
-
-## Add z-score on the data_frame
-z_score <- (spread - mean(spread))/sd(spread)
-
-rates$z_score <- z_score
-head(rates)
-tail(rates)
-model <- lm(mortgage30 ~ treasury10, data = rates)
-lm(model)
-summary(model)
-
-predicted_30y <- predict(model, data.frame(treasury10 = c(4.5)))
-attributes(model)
-
-cor(rates$treasury10, rates$mortgage30)
-model$coefficients
-model$residuals
-model$effects
-model$rank
-model$fitted.values
-
-coefficients(model)
-confint(model)
-anova(model)
-
-rate_stats <- rates %>% 
-  summarise(
-    mean_rate = mean(spread),
-    SD_rate = sd(spread),
-    med_est = median(spread),
-    conf_25 = quantile(spread, 0.25),
-    conf_75 = quantile(spread, 0.75),
-    conf_low = quantile(spread, 0.025),
-    conf_high = quantile(spread, 0.975)
+  # Set FRED API key if needed
+  # fredr_set_key("your_api_key_here")
+  
+  # --- Download Data ---
+  fed_bs <- fredr(series_id = "WALCL") %>%
+    select(date, balance_sheet = value) %>% 
+    filter(date >= "2006-01-05")
+  
+  usrec <- fredr(series_id = "USREC") %>%
+    select(date, recession = value)%>% 
+    filter(date >= "2006-01-05")
+  
+  
+  recessions <- usrec %>%
+    mutate(
+      start = recession == 1 & lag(recession, default = 0) == 0,
+      end   = recession == 0 & lag(recession, default = 0) == 1
+    )
+  
+  rec_start <- recessions %>%
+    filter(start) %>%
+    pull(date)
+  
+  rec_end <- recessions %>%
+    filter(end) %>%
+    pull(date)
+  
+  # If last recession is ongoing, close it with last Fed BS date
+  if(length(rec_end) < length(rec_start)){
+    rec_end <- c(rec_end, max(fed_bs$date))
+  }
+  
+  recession_df <- tibble(
+    start = rec_start,
+    end   = rec_end
   )
+  
+p1 <-   ggplot() +
+    
+    # recession shading
+    geom_rect(data = recession_df,
+              aes(xmin = start, xmax = end,
+                  ymin = -Inf, ymax = Inf),
+              fill = "gray70", alpha = 0.4) +
+    
+    # Fed balance sheet
+    geom_line(data = fed_bs,
+              aes(x = date, y = balance_sheet/1e6),
+              color = "#1f77b4",
+              linewidth = 1) +
+    
+    scale_y_continuous(
+      labels = label_dollar(suffix = "T"),
+      name = "Balance Sheet (Trillions USD)"
+    ) +
+    
+    labs(
+      title = "Federal Reserve Balance Sheet vs Recessions",
+      subtitle = "WALCL with NBER recession shading",
+      caption = "Source: FRED, by Takayuki Tamura"
+    ) +
+    
+    theme_bw(base_size = 14) +
+    theme(panel.grid = element_blank())
 
-conf_low <- rate_stats %>% 
-  select(conf_low)
-conf_high <- rate_stats %>% 
-  select(conf_high)
-
-# Plot summary
-
-latest_spread <- slice_tail(rates)[4]
-
-slice_tail(rates)[4]
-
-
-e <- ggplot(rates, aes(x = spread)) +
-  geom_histogram(fill = "steelblue", color = "white", bins = 30, alpha = .3) +
-  geom_vline(data = rate_stats,
-             aes(xintercept = mean_rate),
-             size = 1, color = "red") +
-  geom_vline(data = rate_stats,
-             aes(xintercept = mean_rate + SD_rate),
-             linetype = "dashed", color = "red") +
-  geom_vline(data = rate_stats,
-             aes(xintercept = mean_rate - SD_rate),
-             linetype = "dashed", color = "red") +
-  geom_vline(data = rate_stats,
-             aes(xintercept = mean_rate + SD_rate*2),
-             linetype = "dotted", color = "red") +
-  geom_vline(data = rate_stats,
-             aes(xintercept = mean_rate - SD_rate*2),
-             linetype = "dotted", color = "red") +
-  scale_x_continuous(limits=c(NA, 4),
-                     breaks = c(0, 4, 2)) +
-  labs(title = "The 30-year mortgage spread over 10y treasury yield",
-      # subtitle = glue("Current spread at {latest_spread}% is outside of the 95% confidence interval(=2SD)"),
-       x = "spread(%)",
-       caption = "Source: FRED(Federal Reserve Economic Data") +
-  theme_classic() +
-  theme(
-    plot.title.position = "plot",
-    plot.title = element_textbox_simple(),
-    plot.subtitle = element_text(face = "italic"
-  ))
-
-e
-
-# Add labels
-z1 <-  round((mean(rates$spread) + sd(rates$spread)),2)
-z2 <- round((mean(rates$spread) + sd(rates$spread)*2),2)
-z3 <- round((mean(rates$spread) - sd(rates$spread)),2)
-z4 <- round((mean(rates$spread) - sd(rates$spread)*2),2)
-
-e + geom_text(aes(x = 1.7, y = 450, label = round(rate_stats$mean_rate, 2))) +
-  geom_text(aes(x = z1, y = 400, label = z1)) +
-  geom_text(aes(x = z2, y = 350, label = z2)) +
-  geom_text(aes(x = z3, y = 400, label = z3)) +
-  geom_text(aes(x = z4, y = 350, label = z4)) 
-
-ggsave("figures/30y_mortgage_spread_10TB.png", height = 5, width = 5)
-
-e + geom_text(aes(x = 1.7, y = 470, label = glue("mean = {round(rate_stats$mean_rate, 2)}"))) +
-  geom_text(aes(x = z1 + 0.5, y = 425, label = glue("mean + SD = {z1}"))) +
-  geom_text(aes(x = z2, y = 350, label = glue("mean + 2SD = {z2}"))) +
-  geom_text(aes(x = z3 -0.7, y = 400, label = glue("mean - SD = {z3}"))) +
-  geom_text(aes(x = z4, y = 350, label = glue("mean - 2SD = {z4}"))) 
-
-ggsave("figures/30y_mortgage_spread_10TB.png", height = 5, width = 6)
-
-
-f <- ggplot(rates, aes(x = spread)) +
-  geom_histogram(fill = "blue", color = "white") +
-  geom_rect(data = rate_stats,
-            aes(
-              ymin = 0, ymax = Inf,
-              x = med_est, xmin = conf_low, xmax = conf_high),
-            alpha = 0.2, fill = "green"
-  ) +
-  geom_vline(data = rate_stats, aes(
-    xintercept = med_est),
-    size = 1, color = "red"
-  ) +
-  labs(title = "Histogram - how the spread is distributed",
-  subtitle = glue("95% confidence interval is the stread between {conf_low}% and {conf_high}%"),
-  caption = "FRED(Federal Reserve Econimic Data"
-  ) +
-  theme(
-    plot.title.position = "plot",
-    plot.subtitle = element_text(face = "italic")
-  )
-f
-ggsave("figures/histogram_stread.png", width = 5, height = 5)
-
-
-
-
+p / p1 + plot_layout(heights = c(4, 1))
 
