7eaf47b6d4afae84f8e75dde3819fc3ec53b5239
ttamura01git config --global user.email ttamura010164@me.com~git remote add origin https://github.com/ttamura01/git-fed_rate_cut.gitgit push origin mainls <ttamura010164@me.com>
2026-05-01 07:30:45 -0400

    updates with the latest observations

--- risk_prem_mgg_hy.R
+++ risk_prem_mgg_hy.R
@@ -236,7 +236,7 @@ spread_comp %>%
                      breaks = seq(1.5, 6.0, 1),
                      labels = label_comma(accuracy = 0.1)) +
   labs(title = "Risk Premium of US 30-Year Mortgage and High-Yield Bonds",
-       subtitle = "The BAML High-Yield OAS(Option-Adjusted Spread) started compressing",
+       subtitle = "The BAML High-Yield OAS(Option-Adjusted Spread) already declined to the pre-Iran conflict level",
        caption = "FRED(Federal Reserve Economic Data), WSJ, by Takayuki Tamura",
        x = NULL, y = NULL) +
   theme(
